d57b76933a2be9cbad2027e33f36434627bf6a3b
Sachanee Madhukala <120290210+Sachanee@users.noreply.github.com>
2023-11-23 18:38:48 +0530

    Connect mysql and create home file

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+    content = models.TextField(max_length=1000)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.content[:5]
+
+    @property
+    def number_of_comments(self):
+        return Comment.objects.filter(post_connected=self).count()
