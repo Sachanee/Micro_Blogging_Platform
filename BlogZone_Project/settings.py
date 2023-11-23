165dbff53cc3fe555764654bbc1cc5f4d26920f8
Sachanee Madhukala <120290210+Sachanee@users.noreply.github.com>
2023-11-23 21:02:51 +0530

    create a .env file and hided pw

--- BlogZone_Project/settings.py
+++ BlogZone_Project/settings.py
@@ -1,8 +1,10 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -67,7 +69,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "blogzonedb",
         "USER": "root",
-        "PASSWORD": "s@12345Sm",
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
         "HOST": "localhost",
         "PORT": "3306",
     }
