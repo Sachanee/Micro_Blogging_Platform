7dc6ded84e7189a64728cc70f1cbebbd30b7d112
Sithumini <147580513+sithumini50@users.noreply.github.com>
2023-11-24 08:31:04 +0530

    settings

--- BlogZone_Project/settings.py
+++ BlogZone_Project/settings.py
@@ -1,6 +1,10 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import pymysql 
+
+pymysql.version_info = (1,4,6,'final',0)
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
