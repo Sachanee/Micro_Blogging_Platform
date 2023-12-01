3d007cf4cd1ceecd2aefa25fff3355aa5f0e8bb4
Sachanee Madhukala <120290210+Sachanee@users.noreply.github.com>
2023-12-01 20:47:01 +0530

    Add views and urls to the about.html page

--- Blog/views.py
+++ Blog/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "Blog/home.html")
+
+
+def about(request):
+    return render(
+        request,
+        "Blog/about.html",
+    )
