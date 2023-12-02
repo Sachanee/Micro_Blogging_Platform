e50a34bf453d0e10194395a398484967fc1a816c
Sithumini <147580513+sithumini50@users.noreply.github.com>
2023-12-02 16:29:18 +0530

    added cryspy.py to settings.

--- BlogZone_Project/settings.py
+++ BlogZone_Project/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "Blog",
     "users",
+    "crispy_forms"
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
