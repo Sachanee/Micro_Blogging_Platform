50412ac552e86c0f3f8852847b8c348d18dd1848
YasidaWanigatunga <yasidawani12@gmail.com>
2023-12-03 11:54:35 +0530

    update urls.py

--- BlogZone_Project/urls.py
+++ BlogZone_Project/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("Blog.urls")),
+    path('login/', auth_views.LoginView
+         .as_view(template_name='users/login.html'), name='login'),
 ]
