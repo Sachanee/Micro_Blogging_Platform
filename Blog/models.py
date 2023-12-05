022f227ce4538f27e7f79c94aff28628c1249c5d
DarshanaMadushanka <darshanamadushanka47@gmail.com>
2023-12-05 22:47:59 +0530

    update models

--- Blog/models.py
+++ Blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name="liked_posts", blank=True)
-    image = models.imageField(default='default.png', upload_to='image_posts', blank=True)
+    post_image = models.ImageField(upload_to="post_images", blank=True)
 
     def __str__(self):
         return self.content[:4]
